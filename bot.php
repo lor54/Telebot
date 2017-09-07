ea92f5736b3e070d480494793cff5687fca109bf
Lorenzo <lor54campion@gmail.com>
2017-09-07 15:44:00 +0200

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,13 @@
+<?php
+
+// YOUR BOT TOKEN, REQUEST ONE OF IT ON @BotFather
+define('token', '111111111:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+//INCLUDE TELEBOT
+include 'base.php';
+
+
+// BOT CODE
+if ($text == "/start")
+{
+	send($cid, "Hi! Welcome on TeleBOT!);
+}
